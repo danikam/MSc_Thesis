0fedb0b06e012f513ddbc5f5af3074ec20b6bfc9
damacdon <danikam1@uvic.ca>
2021-02-18 17:28:59 -0800

    Init commit

---
+++ MakePdf.sh
@@ -0,0 +1,9 @@
+rm DMacDonell_thesis.aux
+rm DMacDonell_thesis.lof
+rm DMacDonell_thesis.log
+rm DMacDonell_thesis.lop
+rm DMacDonell_thesis.lot
+rm DMacDonell_thesis.out
+rm DMacDonell_thesis.toc
+pdflatex DMacDonell_thesis.tex
+pdflatex DMacDonell_thesis.tex
